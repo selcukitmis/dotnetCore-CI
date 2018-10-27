01e99714afdf3103a95af563f5692b55ae003458
Selçuk İTMİŞ <slck87@hotmail.com>
2018-10-27 22:54:56 +0300

    initial commit

---
+++ api.Tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace api.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        [Owner("Selçuk")]
+        public void TestMethod1()
+        {
+            var result = 1;
+            Assert.AreEqual(1,1);
+        }
+    }
+}
